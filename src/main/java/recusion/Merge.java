223b67445176b74966599f2d4c101648523dadc8
shaid <shaid@gappsexperts.com>
2020-02-28 22:52:38 +0530

    done

--- src/main/java/recusion/Merge.java
+++ src/main/java/recusion/Merge.java
@@ -1,4 +1,5 @@
 package recusion;
+
 import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
@@ -7,32 +8,54 @@ import static java.util.Arrays.sort;
 
 public class Merge {
     public static void main(String[] args) {
-        int a[] = {2, 6};
-        int b[] = {4, 1};
-        int[] merge = merge(a, 0, 1, b, 0, 1);
-        System.out.println(Arrays.toString(merge));
+        int arr[] = {1, 3, 5, 2};
+        merger(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static int[] merge(int[] a, int x1, int x2, int[] b, int y1, int y2) {
-        sort(a);
-        sort(b);
+    private static void merger(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            merger(arr, start, mid);
+            merger(arr, mid + 1, end);
+            merge(arr, start, mid, end);
+        }
+    }
 
-        int[] abc = new int[4];
-        int k = 0;
-        while (x1 <= x2 && y1 <= y2) {
-            if (a[x1] <= b[y1]) {
-                abc[k++] = a[x1++];
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int n1 = mid - start + 1;
+        int n2 = end - mid;
+
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[start + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[mid + 1 + j];
+
+        System.out.println(Arrays.toString(L));
+        System.out.println(Arrays.toString(R));
+        System.out.println("--------------");
+
+        int k = start;
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
             } else {
-                abc[k++] = b[y1++];
+                arr[k++] = R[j++];
             }
         }
-        while (x1 <= x2) {
-            abc[k++] = a[x1++];
+        while (i < n1) {
+            arr[k++] = L[i++];
         }
 
-        while (y1 <= y2) {
-            abc[k++] = a[y1++];
+        while (j < n2) {
+            arr[k++] = R[j++];
         }
-        return abc;
     }
 }
