6cf983a63b39d6006d056e7c3ee0fe88d4513596
shaid <shaid@gappsexperts.com>
2020-02-29 00:12:56 +0530

    done

--- src/main/java/recusion/Tree.java
+++ src/main/java/recusion/Tree.java
@@ -1,12 +1,36 @@
 package recusion;
 
 class Node {
-    int key;
+    int data;
     Node left, right;
 
     public Node(int item) {
-        key = item;
+        data = item;
         left = right = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
     }
 }
 
@@ -23,21 +47,21 @@ class BinaryTree {
 
         printPostorder(node.left);
         printPostorder(node.right);
-        System.out.print(node.key + " ");
+        System.out.print(node.data + " ");
     }
 
     void printInorder(Node node) {
         if (node == null)
             return;
         printInorder(node.left);
-        System.out.print(node.key + " ");
+        System.out.print(node.data + " ");
         printInorder(node.right);
     }
 
     void printPreorder(Node node) {
         if (node == null)
             return;
-        System.out.print(node.key + " ");
+        System.out.print(node.data + " ");
         printPreorder(node.left);
         printPreorder(node.right);
     }
@@ -61,6 +85,4 @@ public class Tree {
         System.out.println("\nPostorder traversal of binary tree is ");
         tree.printPostorder(tree.root);
     }
-
-
 }
