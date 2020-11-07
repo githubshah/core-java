a68da9fdaa5aa2ae772c9839485fddbcda11516b
shaid hussain <shaid@gappsexperts.com>
2020-11-07 20:14:37 +0530

    refector

--- src/main/java/collectiontest/Main.java
+++ src/main/java/collectiontest/Main.java
@@ -2,30 +2,24 @@ package collectiontest;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Main {
-  public static void main(String[] args) throws ParseException {
-    Set<Integer> integersA = new HashSet<>(Arrays.asList(1, 2));
-    Set<Integer> integersB = new HashSet<>(Arrays.asList(3, 4));
+    public static void main(String[] args) throws ParseException {
+        Set<Integer> integersA = new HashSet<>(Arrays.asList(1, 2));
+        Set<Integer> integersB = new HashSet<>(Arrays.asList(3, 4));
 
-    boolean disjoint = Collections.disjoint(integersA, integersB);
+        boolean disjoint = Collections.disjoint(integersA, integersB);
 
-    System.out.println(integersA);
-    System.out.println(disjoint);
+        System.out.println(integersA);
+        System.out.println(disjoint);
 
-    Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse("2018-07-15");
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse("2018-07-15");
 
-    System.out.println(date1);
-  }
+        System.out.println(date1);
+    }
 }
