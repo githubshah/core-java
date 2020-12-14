fee422826bfca50d0471e0b4890322a61c15158e
shaid-hussain <shaid@gappsexperts.com>
2020-12-14 15:50:47 +0530

    done

--- src/main/java/core/multiThreading/producerconsumer/ProducerConsumerNotifyPattern.java
+++ src/main/java/core/multiThreading/producerconsumer/ProducerConsumerNotifyPattern.java
@@ -1,20 +1,38 @@
 package core.multiThreading.producerconsumer;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Date;
+
+class SharedResource {
+    Long value = 0L;
+    boolean hasValue = false;
+
+    synchronized public Long getLong() {
+        Long temp = this.value;
+        value = 0L; // reset
+        this.hasValue = false;
+        return temp;
+    }
+
+    synchronized public void setLong(Long value) {
+        this.value = value;
+        this.hasValue = true;
+    }
+
+    synchronized public boolean isHasValue() {
+        return hasValue;
+    }
+}
 
 public class ProducerConsumerNotifyPattern {
 
     public static void main(String args[]) {
 
         //Creating shared object
-        BlockingQueue sharedQueue = new LinkedBlockingQueue();
+        SharedResource sharedResource = new SharedResource();
 
         //Creating Producer and Consumer Thread
-        Thread prodThread = new Thread(new Producer2(sharedQueue));
-        Thread consThread = new Thread(new Consumer2(sharedQueue));
+        Thread prodThread = new Thread(new Producer2(sharedResource));
+        Thread consThread = new Thread(new Consumer2(sharedResource));
 
         //Starting producer and Consumer thread
         prodThread.start();
@@ -26,45 +44,61 @@ public class ProducerConsumerNotifyPattern {
 //Producer Class in java
 class Producer2 implements Runnable {
 
-    private final BlockingQueue sharedQueue;
+    SharedResource sharedResource;
 
-    public Producer2(BlockingQueue sharedQueue) {
-        this.sharedQueue = sharedQueue;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 10; i++) {
-            try {
-                System.out.println("Produced: " + i);
-                sharedQueue.put(i);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer2.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
-}
-
-//Consumer Class in Java
-class Consumer2 implements Runnable {
-
-    private final BlockingQueue sharedQueue;
-
-    public Consumer2(BlockingQueue sharedQueue) {
-        this.sharedQueue = sharedQueue;
+    public Producer2(SharedResource sharedResource) {
+        this.sharedResource = sharedResource;
     }
 
     @Override
     public void run() {
         while (true) {
-            try {
-                System.out.println("Consumed: " + sharedQueue.take());
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Consumer2.class.getName()).log(Level.SEVERE, null, ex);
+            synchronized (sharedResource) {
+                if (!sharedResource.isHasValue()) {
+                    sharedResource.setLong(new Date().getTime());
+                    try {
+                        Thread.sleep(5000);
+                        System.out.println("Produced element and notifying to all");
+                        notifyAll();
+                    } catch (Exception e) {
+                    }
+                } else {
+                    try {
+                        wait();
+                    } catch (Exception e) {
+                    }
+                }
             }
         }
     }
+}
 
+//Consumer Class in Java
+class Consumer2 implements Runnable {
 
+    SharedResource sharedResource;
+
+    public Consumer2(SharedResource sharedResource) {
+        this.sharedResource = sharedResource;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (sharedResource) {
+                if (!sharedResource.isHasValue()) {
+                    try {
+                        wait();
+                    } catch (Exception e) {
+                    }
+                } else {
+                    System.out.println("Consumed: " + sharedResource.getLong());
+                    try {
+                        notifyAll();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+    }
 }
