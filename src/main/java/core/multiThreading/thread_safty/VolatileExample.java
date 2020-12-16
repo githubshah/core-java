eabb45465feb6a9f3d83925de9a8575d1802ff21
shaid-hussain <shaid@gappsexperts.com>
2020-12-16 09:08:27 +0530

    msg

--- src/main/java/core/multiThreading/thread_safty/VolatileExample.java
+++ src/main/java/core/multiThreading/thread_safty/VolatileExample.java
@@ -2,49 +2,61 @@ package core.multiThreading.thread_safty;
 
 public class VolatileExample {
 
-	// Initializing volatile variables
-	// a, b
-	static volatile int a = 0, b = 0;
+    // Initializing volatile variables
+    // a, b
+    static volatile int a = 0, b = 0;
 
-	// Defining a static void method
-	static void method_one()
-	{
-		a++;
-		b++;
-	}
+    // Defining a static void method
+    static void method_one() {
+        int x = a;
+        int y = b;
+        x++;
+        y++;
+        a = x;
+        b = y;
+    }
 
-	// Defining static void method
-	static void method_two()
-	{
-		System.out.println(
-			"a=" + a + " b=" + b);
-	}
+    // Defining static void method
+    static void method_two() {
+        int x = a;
+        int y = b;
+        if (x != y)
+            System.out.println(
+                "a=" + x + " b=" + y);
+    }
 
-	public static void main(String[] args)
-	{
+    public static void main(String[] args) {
 
-		// Creating an instance t1 of
-		// Thread class
-		Thread t1 = new Thread() {
-			public void run()
-			{
-				for (int i = 0; i < 5; i++)
-					method_one();
-			}
-		};
+        // Creating an instance t1 of
+        // Thread class
+        Thread t1 = new Thread() {
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    method_one();
+                    yield();
+                }
+            }
+        };
 
-		// Creating an instance t2 of
-		// Thread class
-		Thread t2 = new Thread() {
-			public void run()
-			{
-				for (int i = 0; i < 5; i++)
-					method_two();
-			}
-		};
+        // Creating an instance t2 of
+        // Thread class
+        Thread t2 = new Thread() {
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    method_two();
+                    yield();
+                }
+            }
+        };
 
-		// Starting instance t1 and t2
-		t1.start();
-		t2.start();
-	}
+        // Starting instance t1 and t2
+        t1.start();
+        t2.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/core/multiThreading/visiblity/solution/VolatileTest.java
+++ src/main/java/core/multiThreading/visiblity/solution/VolatileTest.java
@@ -4,16 +4,16 @@ public class VolatileTest {
     private static volatile int MY_INT = 0;
 
     public static void main(String[] args) {
-        new ChangeListener().start();
-        new ChangeMaker().start();
+        new Consumer().start();
+        new Producer().start();
     }
 
-    static class ChangeListener extends Thread {
+    static class Consumer extends Thread {
         @Override
         public void run() {
-            int local_value = MY_INT;
+            int local_value = MY_INT; // see 12:00, thread A on it
             while (local_value < 5) {
-                if (local_value != MY_INT) {
+                if (local_value != MY_INT) {  // see 12:02 but thread B updated MY_INT value on 12:01
                     System.out.println("Got Change for MY_INT : " + MY_INT);
                     local_value = MY_INT;
                 }
@@ -21,13 +21,13 @@ public class VolatileTest {
         }
     }
 
-    static class ChangeMaker extends Thread {
+    static class Producer extends Thread {
         @Override
         public void run() {
             int local_value = MY_INT;
             while (MY_INT < 5) {
                 System.out.println("Incrementing MY_INT to " + (local_value + 1));
-                MY_INT = ++local_value;
+                MY_INT = ++local_value; // 12:01 thread B updated MY_INT value
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
