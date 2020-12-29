80fde6ce346baf1aba6b49543467dc09bbe40533
shaid-hussain <shaid@gappsexperts.com>
2020-12-29 22:30:10 +0530

    ii

--- src/main/java/learning/PossibleCombinationOfString.java
+++ src/main/java/learning/PossibleCombinationOfString.java
@@ -10,7 +10,8 @@ public class PossibleCombinationOfString {
         long pow_set_size = (long) Math.pow(2, setSize); // possible consecutive char combination
         int counter, j;
 
-        for (counter = 0; counter < pow_set_size; counter++) {
+        for (counter = 1; counter < pow_set_size; counter++) {
+            System.out.print(counter +" = ");
             for (j = 0; j < setSize; j++) {
                 if ((counter & (1 << j)) > 0)
                     System.out.print(set[j]);
