c05cc1f3bbfc677a88b9c5621ed4080da9d7fbfc
shaid-hussain <shaid@gappsexperts.com>
2021-01-05 08:58:51 +0530

    custom executor services

---
+++ src/main/java/learning/core/multiThreading/executorServices/custom/CustomThreadPoolExample.java
@@ -0,0 +1,19 @@
+package learning.core.multiThreading.executorServices.custom;
+
+public class CustomThreadPoolExample
+{
+    public static void main(String[] args) 
+    {
+        CustomThreadPool customThreadPool = new CustomThreadPool(2);
+         
+        for (int i = 1; i <= 5; i++) 
+        {
+            Task task = new Task("Task " + i);
+            System.out.println("Created : " + task.getName());
+ 
+            customThreadPool.execute(task);
+        }
+
+        customThreadPool.shutdown();
+    }
+}
