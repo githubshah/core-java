1557e95d5e0dca412922de12cfffed2a99a89e84
githubshah721 <shaid.hussain0721@gmail.com>
2021-02-19 23:56:32 +0530

    serialize and de-serialize

---
+++ src/main/java/charOccerence/CharOccurrence.java
@@ -0,0 +1,18 @@
+package charOccerence;
+
+public class CharOccurrence {
+
+    public static void main(String[] args) {
+        char[] input = {'a', 'b', 'c', 'a', 'b', 'd', 'a'};
+        System.out.println(findOccurrence(input, 'a'));
+    }
+
+    private static int findOccurrence(char[] input, char searchChar) {
+        int[] output = new int[input.length];
+        for (char value : input) {
+            int index = value - 'a';
+            output[index] = output[index] == 0 ? 1 : ++output[index];
+        }
+        return output[searchChar - 'a'];
+    }
+}
