26cf226f9fb54864e1108510341a3c08b2410f16
githubshah721 <shaid.hussain0721@gmail.com>
2021-02-19 23:34:52 +0530

    serialize and de-serialize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a[] = {1, 2, 4, 4, 5, -3, -1};
+        int a[] = {1, 2, 4, 4, 15, -13, -1, 6, 9};
         int max = a[0], max2 = a[0];
         for (int i = 0; i < a.length; i++) {
             if (a[i] > max2) {
