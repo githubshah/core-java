2902f3264137efd9a1eec7fd6a166f09bcb0216a
githubshah <72010111+githubshah@users.noreply.github.com>
2022-05-19 23:18:54 +0530

    weak hashmap

--- src/main/java/learning/core/stringdemo/StringReplaceAndReplaceAll.java
+++ src/main/java/learning/core/stringdemo/StringReplaceAndReplaceAll.java
@@ -11,17 +11,6 @@ public class StringReplaceAndReplaceAll {
         repalceOnRegex(); // not works
     }
 
-    private static void repalceOnRegex() {
-        String myString = "__1_6____3__6_345____0";
-
-        String regex = "\\d";
-        String replacement = "x";
-
-        String newString = myString.replace(regex, replacement);
-        // __x_x____x__x_xxx____x
-        System.out.println(newString);
-    }
-
     private static void replaceAll() {
         String myString = "__1_6____3__6_345____0";
 
@@ -30,7 +19,7 @@ public class StringReplaceAndReplaceAll {
 
         String newString = myString.replaceAll(regex, replacement);
         // __x_x____x__x_xxx____x
-        System.out.println(newString);
+        System.out.println("replaceAll: "+newString);
     }
 
     private static void replace() {
@@ -40,5 +29,17 @@ public class StringReplaceAndReplaceAll {
         String replacement = "sheep";
 
         String newString = myString.replace(target, replacement);
+        System.out.println("replace: "+newString);
+    }
+
+    private static void repalceOnRegex() {
+        String myString = "__1_6____3__6_345____0";
+
+        String regex = "\\d";
+        String replacement = "x";
+
+        String newString = myString.replace(regex, replacement);
+        // __x_x____x__x_xxx____x
+        System.out.println("repalceOnRegex: "+newString);
     }
 }
