f776f18e35d20d4ab47c988fd5922820ca0eb90b
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-04 12:02:59 +0530

    doibe

---
+++ src/main/java/learning/aDataStucture/graph/Island.java
@@ -0,0 +1,66 @@
+package learning.aDataStucture.graph;
+
+import java.util.HashSet;
+
+public class Island {
+
+
+    private static boolean countIsLand(char[][] arr, int r, int c, HashSet<String> visited) {
+
+        if (r < 0 || r >= arr.length) {
+            return false;
+        }
+
+        if (c < 0 || c >= arr[0].length) {
+            return false;
+        }
+
+        if (arr[r][c] == 'W') {
+            return false;
+        }
+
+        String pos = r + "," + c;
+        if (visited.contains(pos)) {
+            return false;
+        }
+
+        visited.add(pos);
+
+        countIsLand(arr, r - 1, c, visited);
+        countIsLand(arr, r + 1, c, visited);
+        countIsLand(arr, r, c - 1, visited);
+        countIsLand(arr, r, c + 1, visited);
+        countIsLand(arr, r + 1, c + 1, visited);
+        countIsLand(arr, r - 1, c - 1, visited);
+        countIsLand(arr, r + 1, c - 1, visited);
+        countIsLand(arr, r - 1, c + 1, visited);
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char arr[][] =
+                {
+                        {'W', 'L', 'W', 'W', 'L'},
+                        {'W', 'L', 'W', 'L', 'W'},
+                        {'W', 'W', 'W', 'L', 'W'},
+                        {'W', 'W', 'L', 'L', 'W'},
+                        {'L', 'W', 'W', 'L', 'L'},
+                        {'L', 'L', 'W', 'W', 'W'},
+                };
+
+
+        int count = 0;
+        HashSet<String> visited = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (countIsLand(arr, i, j, visited)) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+
+}
