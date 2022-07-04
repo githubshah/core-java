f08e3d62b98335648cc828a212c21b1b4326efac
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-04 10:18:09 +0530

    doibe

--- src/main/java/learning/aDataStucture/graph/undirected/LargestSpanningTree.java
+++ src/main/java/learning/aDataStucture/graph/undirected/LargestSpanningTree.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class NoOfTree {
+public class LargestSpanningTree {
 
     static int fun(JSONObject jsonObject) {
 
