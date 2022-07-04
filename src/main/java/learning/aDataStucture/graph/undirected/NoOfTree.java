f61a68065a08ff375bdd12c87377d549250efe8e
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-04 10:17:28 +0530

    doibe

---
+++ src/main/java/learning/aDataStucture/graph/undirected/NoOfTree.java
@@ -0,0 +1,49 @@
+package learning.aDataStucture.graph.undirected;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NoOfTree {
+
+    static int fun(JSONObject jsonObject) {
+
+        Set<String> set = new HashSet<>();
+        int longest = 0;
+
+        Iterator<String> keys = jsonObject.keys();
+        while (keys.hasNext()) {
+            int longest1 = longest(jsonObject, keys.next(), set);
+            if(longest< longest1) longest = longest1;
+        }
+        return longest;
+    }
+
+    private static int longest(JSONObject jsonObject, String node, Set<String> visited) {
+        if (visited.contains(node)) {
+            return 0;
+        }
+
+        visited.add(node);
+
+        int size = 1;
+
+        try {
+            for (Object s : (JSONArray) (jsonObject.get(node))) {
+                size += longest(jsonObject, (String) s, visited);
+            }
+        } catch (Exception ignored) {
+        }
+
+        return size;
+    }
+
+    public static void main(String[] args) {
+        String as = "{'0' : ['8', '1', '5'], '1': ['0'], '5': ['0' , '8'], '8': ['0', '5'], '2': ['3' , '4'], '3' : ['2' , '4'], '4' : ['3' , '2']}";
+        JSONObject jsonObject = new JSONObject(as);
+        System.out.println(fun(jsonObject));
+    }
+}
