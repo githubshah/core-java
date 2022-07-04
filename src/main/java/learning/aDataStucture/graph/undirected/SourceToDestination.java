6a0d668a105dd9ee48706623b577e251dd26f1af
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-04 09:40:38 +0530

    doibe

--- src/main/java/learning/aDataStucture/graph/undirected/SourceToDestination.java
+++ src/main/java/learning/aDataStucture/graph/undirected/SourceToDestination.java
@@ -3,17 +3,23 @@ package learning.aDataStucture.graph.undirected;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SourceToDestination {
 
-    static boolean fun(JSONObject jsonObject, String source, String destination) {
+    static boolean fun(JSONObject jsonObject, String source, String destination, Set<String> set) {
+
+        if(set.contains(source))  return false;
+
+        set.add(source);
 
         if (source.equals(destination)) {
             return true;
         }
-        System.out.println(source);
         try {
             for (Object s : (JSONArray) (jsonObject.get(source))) {
-                if (fun(jsonObject, (String) s, destination)) {
+                if (fun(jsonObject, (String) s, destination, set)) {
                     return true;
                 }
             }
@@ -24,9 +30,59 @@ public class SourceToDestination {
     }
 
     public static void main(String[] args) {
-        String as = "{'a' : ['b', 'c'], 'b': ['d'], 'c': ['e'], 'd': ['f'], 'e': [], 'f' : [], 'h' : []}";
-        JSONObject jsonObject = new JSONObject(as);
-        System.out.println(fun(jsonObject, "a", "f"));
-        System.out.println(fun(jsonObject, "a", "h"));
+        String as = "[['w', 'x'], ['x','y'], ['y1', 'z']]";
+        JSONArray jsonObject = new JSONArray(as);
+
+        JSONObject object = build(jsonObject);
+
+        //System.out.println(object);
+
+        System.out.println(fun(object, "w", "y", new HashSet()));
+        System.out.println(fun(object, "w", "z", new HashSet()));
+    }
+
+    private static JSONObject build(JSONArray jsonArray) {
+
+        JSONObject jsonObject = new JSONObject();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONArray o = (JSONArray) jsonArray.get(i);
+            String a = null;
+            String b = null;
+            a = (String) o.get(0);
+            b = (String) o.get(1);
+
+            boolean b1 = contains(jsonObject, a);
+            if (!b1) {
+                jsonObject.put(a, new JSONArray());
+            }
+
+            if (!contains(jsonObject, b)) {
+                jsonObject.put(b, new JSONArray());
+            }
+
+            try {
+                JSONArray o1 = (JSONArray) jsonObject.get(a);
+                o1.put(b);
+            } catch (Exception e) {
+            }
+
+            try {
+                JSONArray o2 = (JSONArray) jsonObject.get(b);
+                o2.put(a);
+            } catch (Exception e) {
+            }
+
+        }
+        return jsonObject;
+    }
+
+    private static boolean contains(JSONObject o, String a) {
+        try {
+            Object o1 = o.get(a);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
