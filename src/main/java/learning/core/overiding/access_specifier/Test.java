5e17d5576e81ce0552738fcf68bd36849c446d18
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-07 11:06:24 +0530

    doibe

---
+++ src/main/java/learning/core/overiding/access_specifier/Test.java
@@ -0,0 +1,26 @@
+package learning.core.overiding.access_specifier;
+
+
+class Base {
+    protected void fun() {
+        System.out.println("base");
+
+    }
+}
+
+class Child extends Base {
+
+    @Override
+    public void fun() {
+        System.out.println("child");
+    }
+}
+
+public class Test {
+
+    public static void main(String[] args) {
+        Base base = new Child();
+
+        base.fun();
+    }
+}
