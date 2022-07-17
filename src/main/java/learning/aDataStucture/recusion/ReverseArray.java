f23cfb5d785d523b64044339c729c22a65510402
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-17 21:56:28 +0530

    done

---
+++ src/main/java/learning/aDataStucture/recusion/ReverseArray.java
@@ -0,0 +1,27 @@
+package learning.aDataStucture.recusion;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+        new ReverseArray().fun(arr, -1);
+
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+    private void fun(int[] arr, int i) {
+
+        if (i == arr.length - 1) {
+            return;
+        }
+
+        i++;
+        int x = arr[i];
+        fun(arr, i);
+        arr[arr.length - i - 1] = x;
+    }
+
+}
