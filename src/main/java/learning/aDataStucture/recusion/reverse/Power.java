654dcb08e413f5a35732bad7236608ba3389fbef
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-18 09:13:44 +0530

    done

---
+++ src/main/java/learning/aDataStucture/recusion/reverse/Power.java
@@ -0,0 +1,20 @@
+package learning.aDataStucture.recusion.reverse;
+
+public class Power {
+
+    public static void main(String[] args) {
+
+        int s = 123;
+        int fun = new Power().fun(2, 4);
+
+        System.out.println(fun);
+    }
+
+    private int fun(int num, int power) {  // 2, 3
+
+        if (power == 1) return num;
+        return fun(num, power - 1) * num;
+
+    }
+
+}
