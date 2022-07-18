820f5fa5c980c8f0b8c5279e7c251fdba26b597f
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-18 12:31:58 +0530

    done

--- src/main/java/learning/aDataStucture/graph/longestPathFromSourceToDestiMetrix.java
+++ src/main/java/learning/aDataStucture/graph/longestPathFromSourceToDestiMetrix.java
@@ -4,40 +4,40 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Main {
-    private static boolean isSafe(int[][] mat, Set<String> visitor, int x, int y) {
-        return (x >= 0 && x < mat.length && y >= 0 && y < mat[0].length) &&
-                mat[x][y] == 1 && !visitor.contains(x + "," + y);
+    private static boolean isSafe(int[][] mat, Set<String> visitor, int i, int j) {
+        return (i >= 0 && i < mat.length && j >= 0 && j < mat[0].length) &&
+                mat[i][j] == 1 && !visitor.contains(i + "," + j);
     }
 
     public static int findLongestPath(int[][] mat, Set<String> visited, int i, int j,
                                       int x, int y, int max_dist, int dist) {
-        // if the destination is not possible from the current cell
-        if (mat[i][j] == 0) {
-            return 0;
-        }
 
-        // if the destination is found, update `max_dist`
         if (i == x && j == y) {
             return Integer.max(dist, max_dist);
         }
 
+        if (i < 0 || i >= mat.length) {
+            return max_dist;
+        }
+
+        if (j < 0 || j >= mat[0].length) {
+            return max_dist;
+        }
+
+        if (mat[i][j] == 0) {
+            return max_dist;
+        }
+
+        if (visited.contains(i + "," + j)) {
+            return max_dist;
+        }
+
         visited.add(i + "," + j);
 
-        if (isSafe(mat, visited, i + 1, j)) {
-            max_dist = findLongestPath(mat, visited, i + 1, j, x, y, max_dist, dist + 1);
-        }
-
-        if (isSafe(mat, visited, i, j + 1)) {
-            max_dist = findLongestPath(mat, visited, i, j + 1, x, y, max_dist, dist + 1);
-        }
-
-        if (isSafe(mat, visited, i - 1, j)) {
-            max_dist = findLongestPath(mat, visited, i - 1, j, x, y, max_dist, dist + 1);
-        }
-
-        if (isSafe(mat, visited, i, j - 1)) {
-            max_dist = findLongestPath(mat, visited, i, j - 1, x, y, max_dist, dist + 1);
-        }
+        max_dist = findLongestPath(mat, visited, i + 1, j, x, y, max_dist, dist + 1);
+        max_dist = findLongestPath(mat, visited, i, j + 1, x, y, max_dist, dist + 1);
+        max_dist = findLongestPath(mat, visited, i - 1, j, x, y, max_dist, dist + 1);
+        max_dist = findLongestPath(mat, visited, i, j - 1, x, y, max_dist, dist + 1);
 
         visited.remove(i + "," + j);
 
