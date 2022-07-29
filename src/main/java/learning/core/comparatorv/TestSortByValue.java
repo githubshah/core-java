13131acd1ab68710322b8e0864b814afeba1a935
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-29 08:54:23 +0530

    done

---
+++ src/main/java/learning/core/comparatorv/TestSortByValue.java
@@ -0,0 +1,27 @@
+package learning.core.comparatorv;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class TestSortByValue {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+
+        map.put("C", "AA");
+        map.put("B", "AB");
+        map.put("A", "AC");
+
+        Set<Entry<String, String>> entries = map.entrySet();
+
+        List<Entry<String, String>> list = new ArrayList<>(entries);
+
+        list.sort(Entry.<String, String>comparingByValue().thenComparing(Entry::getKey));
+
+        map.clear();
+
+        list.forEach(x -> map.put(x.getKey(), x.getValue()));
+
+        System.out.println(map);
+    }
+}
