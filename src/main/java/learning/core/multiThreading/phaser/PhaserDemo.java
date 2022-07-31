890395a9031859ff95096899a627ecf394081272
githubshah <72010111+githubshah@users.noreply.github.com>
2022-07-31 22:50:20 +0530

    done

--- src/main/java/learning/core/multiThreading/phaser/PhaserDemo.java
+++ src/main/java/learning/core/multiThreading/phaser/PhaserDemo.java
@@ -2,42 +2,65 @@ package learning.core.multiThreading.phaser;
 
 import java.util.concurrent.Phaser;
 
+import static java.lang.Thread.sleep;
+
 public class PhaserDemo {
+    private static final int PARTIES = 3;
+    private static final int ITERATIONS = 3;
 
-    public static void main(String[] args) {
-        Phaser phaser = new Phaser(1);
+    public static void main(String[] args) throws InterruptedException {
+        Phaser phaser = new Phaser(1) {
+            final private int maxPhase = ITERATIONS;
 
-        new Thread(new Task("thread-9", phaser)).start();
-        new Thread(new Task("thread-10", phaser)).start();
-
-        phaser.arriveAndAwaitAdvance();
-        System.out.println("phase " + phaser.getPhase() + " completed");
-        phaser.arriveAndAwaitAdvance();
-        System.out.println("phase " + phaser.getPhase() + " completed");
-        phaser.arriveAndAwaitAdvance();
-        System.out.println("phase " + phaser.getPhase() + " completed");
+            @Override
+            protected boolean onAdvance(int phase, int registeredParties) {
+                return (phase >= maxPhase - 1) || (registeredParties == 0);
+            }
+        };
+        System.out.printf("after constructor ", phaser);
+        for (int p = 0; p < PARTIES; p++) {
+            int delay = p + 1;
+            Runnable task = new Worker(delay, phaser);
+            new Thread(task).start();
+        }
+        System.out.printf("all threads waiting to start ", phaser);
+        sleep(1);
+        System.out.printf("before all threads started ", phaser);
+        phaser.arriveAndDeregister();
+        System.out.printf("after all threads started ", phaser);
+        phaser.register();
+        while (!phaser.isTerminated()) {
+            phaser.arriveAndAwaitAdvance();
+        }
+        System.out.printf("all threads finished ", phaser);
     }
-}
 
-class Task implements Runnable {
+    private static class Worker implements Runnable {
+        private final int delay;
+        private final Phaser phaser;
 
-    Phaser phaser;
-    String name;
+        Worker(int delay, Phaser phaser) {
+            phaser.register();
+            this.delay = delay;
+            this.phaser = phaser;
+        }
 
-    public Task(String name, Phaser phaser) {
-        this.name = name;
-        this.phaser = phaser;
-    }
+        @Override
+        public void run() {
+            do {
+                work();
+                phaser.arriveAndAwaitAdvance();
+            } while (!phaser.isTerminated());
+        }
 
-    @Override
-    public void run() {
-        System.out.println(name + " hello");
-        this.phaser.arriveAndAwaitAdvance();
-
-        System.out.println(name + " hello again");
-        this.phaser.arriveAndAwaitAdvance();
-
-        System.out.println(name + " hello yupp");
-        this.phaser.arriveAndAwaitAdvance();
+        void work() {
+            System.out.printf("work %d started%n", delay);
+            try {
+                sleep(delay);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("work %d finished%n", delay);
+        }
     }
 }
---
+++ src/main/java/learning/pattern/singleton/nested/SingletonDemo.java
@@ -0,0 +1,59 @@
+package learning.pattern.singleton.nested;// Java Program to demonstrate Thread-Safety
+// in NestedInitialization
+
+class Singleton {
+
+	static {
+		System.out.println("outer static class load");
+	}
+
+	{
+		System.out.println("singleton class constructor is going to call...");
+	}
+
+	private Singleton()
+	{
+		System.out.println("Singleton constructor call, Object created");
+	}
+
+	// This method returns Object, does not create it
+	// Object is created on initialization of Nested class
+	// which happens only once.
+	public static Singleton getInstance()
+	{
+		return Nested.Instance;
+	}
+
+	private static class Nested {
+		static Singleton Instance = new Singleton();
+
+		static {
+			System.out.println("inner static class load");
+		}
+	}
+
+}
+
+public class SingletonDemo {
+	public static void main(String[] args)
+	{
+
+		// Thread 1 will call getInstance
+		Thread t1 = new Thread(() -> {
+			Singleton a = Singleton.getInstance();
+			System.out.println(a);
+		});
+
+		// Thread 2 will also call getInstance
+		Thread t2 = new Thread(() -> {
+			Singleton b = Singleton.getInstance();
+			System.out.println(b);
+		});
+
+		// Start both the Threads
+		t1.start();
+		t2.start();
+
+
+	}
+}
