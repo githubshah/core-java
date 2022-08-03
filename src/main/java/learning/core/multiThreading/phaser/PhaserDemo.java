31d2c5942f1133709caf2bfe14239ae4b4bff6be
githubshah <72010111+githubshah@users.noreply.github.com>
2022-08-03 12:39:52 +0530

    done

--- src/main/java/learning/core/multiThreading/phaser/PhaserDemo.java
+++ src/main/java/learning/core/multiThreading/phaser/PhaserDemo.java
@@ -2,37 +2,22 @@ package learning.core.multiThreading.phaser;
 
 import java.util.concurrent.Phaser;
 
-import static java.lang.Thread.sleep;
-
 public class PhaserDemo {
-    private static final int PARTIES = 3;
-    private static final int ITERATIONS = 3;
 
     public static void main(String[] args) throws InterruptedException {
-        Phaser phaser = new Phaser(1) {
-            final private int maxPhase = ITERATIONS;
+        Phaser phaser = new Phaser(1);
 
-            @Override
-            protected boolean onAdvance(int phase, int registeredParties) {
-                return (phase >= maxPhase - 1) || (registeredParties == 0);
-            }
-        };
-        System.out.printf("after constructor ", phaser);
-        for (int p = 0; p < PARTIES; p++) {
+        System.out.printf("after constructor \n", phaser);
+        for (int p = 0; p < 5; p++) {
             int delay = p + 1;
             Runnable task = new Worker(delay, phaser);
             new Thread(task).start();
         }
-        System.out.printf("all threads waiting to start ", phaser);
-        sleep(1);
-        System.out.printf("before all threads started ", phaser);
-        phaser.arriveAndDeregister();
-        System.out.printf("after all threads started ", phaser);
-        phaser.register();
-        while (!phaser.isTerminated()) {
-            phaser.arriveAndAwaitAdvance();
-        }
-        System.out.printf("all threads finished ", phaser);
+
+        System.out.printf("%s thread going to wait for other threads...%s\n", Thread.currentThread().getName(), phaser);
+        phaser.arriveAndAwaitAdvance();
+        System.out.printf("....................phaser arrived...... %s \n", phaser);
+        System.out.printf("all threads finished %s \n", phaser);
     }
 
     private static class Worker implements Runnable {
@@ -47,20 +32,16 @@ public class PhaserDemo {
 
         @Override
         public void run() {
-            do {
-                work();
-                phaser.arriveAndAwaitAdvance();
-            } while (!phaser.isTerminated());
+            work();
+            System.out.printf("..and %s going to wait for other threads...%s\n", Thread.currentThread().getName(), phaser);
+            phaser.arriveAndAwaitAdvance();
+            System.out.printf("%s release lock...%s\n", Thread.currentThread().getName(), phaser);
+
+
         }
 
         void work() {
-            System.out.printf("work %d started%n", delay);
-            try {
-                sleep(delay);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.printf("work %d finished%n", delay);
+            System.out.printf("%s doing some work", Thread.currentThread().getName(), delay);
         }
     }
 }
