a9634f84e75c760f506bf8e5d6f9db3a1def922f
githubshah <72010111+githubshah@users.noreply.github.com>
2022-08-18 20:31:09 +0530

    done

--- src/main/java/learning/core/D_overiding/Overriding_Exceptions.java
+++ src/main/java/learning/core/D_overiding/Overriding_Exceptions.java
@@ -1,2 +1,33 @@
-package learning.core.D_overiding;public class Overriding_Exceptions {
+package learning.core.D_overiding;
+
+
+import java.io.IOException;
+
+class P1 {
+    void childNotForceToThrowException_and_childNotToThrowHigherExceptionThanItsParent() throws IOException {
+
+    }
+
+    void childOnlyThrowRuntimeExceptionIfThereIsNoExceptionThrowByItsParent(){
+
+    }
+}
+
+class p2 extends P1 {
+    @Override
+    void childNotForceToThrowException_and_childNotToThrowHigherExceptionThanItsParent() {
+
+    }
+
+    @Override
+    void childOnlyThrowRuntimeExceptionIfThereIsNoExceptionThrowByItsParent() throws RuntimeException{
+
+    }
+
+}
+
+public class Overriding_Exceptions {
+    public static void main(String[] args) {
+
+    }
 }
