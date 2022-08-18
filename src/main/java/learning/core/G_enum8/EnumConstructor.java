ea18f430741791383379366a322684d88e5d2a52
githubshah <72010111+githubshah@users.noreply.github.com>
2022-08-18 22:29:27 +0530

    done

--- src/main/java/learning/core/G_enum8/EnumConstructor.java
+++ src/main/java/learning/core/G_enum8/EnumConstructor.java
@@ -1,6 +1,20 @@
 package learning.core.G_enum8;
 
-enum Size {
+enum MaleSize {
+    SMALL("The size is small.");
+
+    private final String size;
+
+    MaleSize(String x) {
+        this.size = x;
+    }
+
+    public String getSize() {
+        return size;
+    }
+}
+
+enum FemaleSize {
 
     // enum constants calling the enum constructors
     SMALL("The size is small."),
@@ -8,15 +22,15 @@ enum Size {
     LARGE("The size is large."),
     EXTRALARGE("The size is extra large.");
 
-    private final String pizzaSize;
+    private final String size;
 
     // private enum constructor
-    private Size(String pizzaSize) {
-        this.pizzaSize = pizzaSize;
+    private FemaleSize(String pizzaSize) {
+        this.size = pizzaSize;
     }
 
     public String getSize() {
-        return pizzaSize;
+        return size;
     }
 }
 
@@ -24,9 +38,19 @@ public class EnumConstructor {
 
     public static void main(String[] args) {
 
-        Size size = Size.SMALL;
-        System.out.println(size.getSize());
+        FemaleSize female = FemaleSize.SMALL;
+        System.out.println(female.getSize());
 
+        FemaleSize femaleSize = null;
+
+        System.out.println(femaleSize.equals(female)); // null pointer exception
+        System.out.println(femaleSize == female);
+
+        MaleSize male = MaleSize.SMALL;
+        System.out.println(male.getSize());
+
+        System.out.println(female.equals(male));
+        //System.out.println(female == male); // compile time type compatible error
 
     }
 
