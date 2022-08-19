43f75ea6e29ae6d8c0bad7083e5a6954a269f17b
githubshah <72010111+githubshah@users.noreply.github.com>
2022-08-19 20:12:41 +0530

    hashMao

--- src/main/java/learning/core/III_comparatorv/TestSortByValue.java
+++ src/main/java/learning/core/III_comparatorv/TestSortByValue.java
@@ -8,15 +8,25 @@ public class TestSortByValue {
     public static void main(String[] args) {
         Map<String, String> map = new LinkedHashMap<>();
 
-        map.put("C", "AA");
-        map.put("B", "AB");
-        map.put("A", "AC");
+        map.put("AC", "AA");
+        map.put("ZB", "AB");
+        map.put("CC", "AB");
+        map.put("CB", "BC");
+        map.put("CA", "BC");
 
         Set<Entry<String, String>> entries = map.entrySet();
 
         List<Entry<String, String>> list = new ArrayList<>(entries);
 
-        list.sort(Entry.<String, String>comparingByValue().thenComparing(Entry::getKey));
+
+        Comparator<Entry<String, String>> entryComparator =
+                Entry.<String, String>comparingByValue().thenComparing(Entry::getKey);
+
+        list.sort((o1, o2) -> {
+            int i1 = o1.getKey().compareTo(o2.getKey());
+            int i2 = o1.getValue().compareTo(o2.getValue());
+            return i2 != 0 ? i2 : i1;
+        });
 
         map.clear();
 
