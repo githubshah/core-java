70d93b98a0c1b862cd554ced723d090a0f430d04
githubshah <72010111+githubshah@users.noreply.github.com>
2022-08-20 09:22:37 +0530

    dobne

--- src/main/java/learning/core/H_collectiontest/list/ArraysAsList.java
+++ src/main/java/learning/core/H_collectiontest/list/ArraysAsList.java
@@ -3,6 +3,7 @@ package learning.core.H_collectiontest.list;
 import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 public class ArraysAsList {
 
@@ -14,6 +15,21 @@ public class ArraysAsList {
 
         integers1.set(0,100);
         System.out.println(integers1);
+
+
+        Integer []arr1 = {1, 2, 3, 1, 2, 3, 4, 2, 3, 4, 3, 4, 4, 5, 6, 7, 8, 9, 10};
+        List<Integer> integers = Arrays.asList(arr1);
+
+
+        int []arr2 = {1, 2, 3, 1, 2, 3, 4, 2, 3, 4, 3, 4, 4, 5, 6, 7, 8, 9, 10};
+        // not possible for primitive type
+        List<int[]> ints = Arrays.asList(arr2);
+
+        // java 8
+        List<Integer> collect = Arrays.stream(arr2)
+                .boxed()
+                .collect(Collectors.toList());
+
 
     }
 }
