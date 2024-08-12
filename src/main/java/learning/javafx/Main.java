c7af05f77afdc2f03ff06c9a3dadc7cb770a1147
Shaid Hussain <shaid.hussain@nagarro.com>
2024-08-12 14:30:31 +0530

    done

--- src/main/java/learning/javafx/Main.java
+++
@@ -1,8 +0,0 @@
-package learning.javafx;
-
-public class Main {
-    public static void main(String[] args) {
-//        JMF11Applet jmf11Applet = new JMF11Applet();
-//        jmf11Applet.init();
-    }
-}
