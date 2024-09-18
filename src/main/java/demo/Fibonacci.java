3051adc6fd32debcddf84cbc2c9959020e90142c
Shaid Hussain <shaid.hussain@nagarro.com>
2024-09-18 12:32:46 +0530

    fixed fibonacci

--- src/main/java/demo/Fibonacci.java
+++ src/main/java/demo/Fibonacci.java
@@ -3,11 +3,11 @@ package demo;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(new Fibonacci().fibonacci(5));
+        System.out.println(new Fibonacci().fibonacci(10));
     }//1,2,3,5,8
 
     private int fibonacci(int number) {
-        if (number == 0 || number == 1) return 1;
+        if (number == 0 || number == 1) return number;
         return fibonacci(number - 1) + fibonacci(number - 2);
     }
 }
