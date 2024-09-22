df3b0787d6f5cad1b52748bd50d77e4c9e1b5d41
Shaid Hussain <shaid.hussain@nagarro.com>
2024-09-22 17:48:43 +0530

    done

--- src/main/java/learning/aDataStucture/recusion/SumOfDigits.java
+++ src/main/java/learning/aDataStucture/recusion/SumOfDigits.java
@@ -3,18 +3,15 @@ package learning.aDataStucture.recusion;
 public class SumOfDigits {
 
     public static void main(String[] args) {
-
-        int fun = new SumOfDigits().fun(468);
-
+        int fun = new SumOfDigits().fun(1234);
         System.out.println(fun);
     }
 
     private int fun(int num) {
-
-        if (num == 0) return 0;
-        int rem = num % 10;
-        return fun(num / 10) + rem;
-
+        if (num == 0) {
+            return 0;
+        }
+        return (num % 10) + fun(num / 10);
     }
 
 }
