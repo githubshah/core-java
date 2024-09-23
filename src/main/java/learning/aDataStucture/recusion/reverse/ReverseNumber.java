bd1b9a4957210cec4c242aad573113d405fd8bcb
Shaid Hussain <shaid.hussain@nagarro.com>
2024-09-23 10:50:03 +0530

    done

--- src/main/java/learning/aDataStucture/recusion/reverse/ReverseNumber.java
+++ src/main/java/learning/aDataStucture/recusion/reverse/ReverseNumber.java
@@ -3,28 +3,16 @@ package learning.aDataStucture.recusion.reverse;
 public class ReverseNumber {
 
     public static void main(String[] args) {
-
         int s = 123;
-        int fun = new ReverseNumber().fun(s, 100);
-
+        int fun = new ReverseNumber().fun(s);
         System.out.println(fun);
     }
 
-    private int fun(int num, int t) {
-
-        /*
-         *  int pre = num / t ;
-         *  int newNum = num - (t * pre)
-         *
-         *  if ( newNum == 0) return pre ;
-         *
-         * */
-
-        if (num == 0) return 0;
-
-        int pre = num / t;
-
-        return fun(num - (t * pre), t / 10) * 10 + pre;
+    private int fun(int num) {
+        if (num < 10) {
+            return num;
+        }
+        return Integer.valueOf(num % 10 + "" + fun(num / 10));
     }
 
 }
