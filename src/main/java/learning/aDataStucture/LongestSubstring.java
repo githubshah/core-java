55ccd3fc04d51b20e9459864a9dcaf02eedabe29
Shaid Hussain <shaid.hussain@nagarro.com>
2024-09-26 00:18:36 +0530

    done

--- src/main/java/learning/aDataStucture/LongestSubstring.java
+++ src/main/java/learning/aDataStucture/LongestSubstring.java
@@ -1,9 +1,26 @@
 package learning.aDataStucture;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LongestSubstring {
+
+    public static int longestUniqueSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {  // abcdaef
+            char currentChar = s.charAt(end);
+            if (map.containsKey(currentChar)) {
+                start = Math.max(start, map.get(currentChar) + 1);
+            }
+            map.put(currentChar, end);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
 
     public static void main(String[] args) {
         String s = "abcdedcba";
